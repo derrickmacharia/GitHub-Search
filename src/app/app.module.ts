a31ae9c39622a468e41e80f914e245b17e8e98af
derrickmacharia <derrick.macharia@student.moringaschool.com>
2021-10-19 19:16:16 +0300

    generate nav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
